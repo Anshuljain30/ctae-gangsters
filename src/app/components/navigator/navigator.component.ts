1e1803dd3eb725be1d6033bb7706c5e905ef5c47
Anshul Jain <anshul24000@gmail.com>
2025-10-04 23:58:52 +0530

    minor fixes

--- src/app/components/navigator/navigator.component.ts
+++ src/app/components/navigator/navigator.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-navigator",
-  templateUrl: "./navigator.component.html",
-  styleUrls: ["./navigator.component.css"],
+  selector: 'app-navigator',
+  templateUrl: './navigator.component.html',
+  styleUrls: ['./navigator.component.css'],
 })
 export class NavigatorComponent implements OnInit {
   collapsed = true;
   names = [
-    "God Puneet",
-    "Legend KK",
-    "Pro Chirag",
-    "Ultimate Bhargav",
-    "Rich Niral",
-    "Brahma Prashant",
-    "One Above All Yogesh",
-    "Great Chotu",
-    "Noob Anshul",
-    "Netawant",
+    'God Puneet',
+    'Legend KK',
+    'Pro Chirag',
+    'Ultimate Bhargav',
+    'Rich Niral',
+    'Brahma Prashant',
+    'One Above All Yogesh',
+    'Great Chotu',
+    'Noob Anshul',
   ];
   caption = [
     `God`,
@@ -31,19 +30,17 @@ export class NavigatorComponent implements OnInit {
     `Joint Man`,
     `Chotu`,
     `Noob`,
-    `Netawant`,
   ];
   images = [
-    "god-puneet.jpg",
-    "legend-kk.jpg",
-    "pro-chirag.jpg",
-    "ultimate-bhargav.jpg",
-    "rich-niral.jpg",
-    "brahma-prashant.jpg",
-    "one-above-all.jpg",
-    "great-chotu.jpg",
-    "noob-anshul.jpg",
-    "netawant.jpg",
+    'god-puneet.jpg',
+    'legend-kk.jpg',
+    'pro-chirag.jpg',
+    'ultimate-bhargav.jpg',
+    'rich-niral.jpg',
+    'brahma-prashant.jpg',
+    'one-above-all.jpg',
+    'great-chotu.jpg',
+    'noob-anshul.jpg',
   ];
 
   constructor() {}
